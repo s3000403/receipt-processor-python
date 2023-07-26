6bcaa0167c5b0a5881e01aeeaf41ca48487fc01b
Rob Schultz <59187877+s3000403@users.noreply.github.com>
2023-07-25 23:39:47 -0700

    Make separate function for creating receipt bounding box

--- process2.py
+++ process2.py
@@ -9,17 +9,12 @@ import cv2
 
 # Based on following site: https://pyimagesearch.com/2021/10/27/automatically-ocring-receipts-and-scans/
 
-def extract_receipt(filename, 
-                    blur_size=5, 
-                    canny_min_thresh=75, 
-                    canny_max_thresh=200,
-                    resize_width = 500,
-                    ):
-    debug = 0
-
-    # load the input image from disk
-    orig = open_image(filename, astype='cv')
-
+def identify_receipt_bounds(orig, 
+                            blur_size=5, 
+                            canny_min_thresh=75, 
+                            canny_max_thresh=200,
+                            resize_width = 500,
+                            ):
     # Resize image, and compute the ratio of the *new* width to the 
     # *old* width
     image = orig.copy()
@@ -40,7 +35,7 @@ def extract_receipt(filename,
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
 
     # initialize a contour that corresponds to the receipt outline
-    receiptCnt = None
+    receiptCnt = np.array([])
     # loop over the contours
     for c in cnts:
         # approximate the contour
@@ -53,32 +48,48 @@ def extract_receipt(filename,
         if len(approx) == 4:
             receiptCnt = approx
             break
+    if len(receiptCnt)==0:
+        return receiptCnt
+    receiptBounds = receiptCnt.reshape(4, 2) * ratio
+    return receiptBounds
+    
+def extract_receipt(filename, 
+                    blur_size=5, 
+                    canny_min_thresh=75, 
+                    canny_max_thresh=200,
+                    resize_width = 500,
+                    ):
+    debug = 1
+
+    # load the input image from disk
+    orig = open_image(filename, astype='cv')
+
+    receiptBounds = identify_receipt_bounds(orig, 
+                    blur_size=blur_size, 
+                    canny_min_thresh=canny_min_thresh, 
+                    canny_max_thresh=canny_max_thresh,
+                    resize_width = resize_width,
+                    )
+    print(len(receiptBounds))
     # if the receipt contour is empty then our script could not find the
     # outline and we should be notified
-    if receiptCnt is None:
+    if len(receiptBounds)==0:
         return np.ndarray((0,0,3))
-#        raise Exception(("Could not find receipt outline. "
-#            "Try debugging your edge detection and contour steps."))
 
     # apply a four-point perspective transform to the *original* image to
     # obtain a top-down bird's-eye view of the receipt
-    receipt = four_point_transform(orig, receiptCnt.reshape(4, 2) * ratio)
+    receipt = four_point_transform(orig, receiptBounds)
     
     # check to see if we should draw the contour of the receipt on the
     # image and then display it to our screen
     if debug > 0:
         print('Plotting debug plots')
-        output = image.copy()
-        cv2.drawContours(output, [receiptCnt], -1, (0, 255, 0), 2)
-        cv2.imshow("Receipt Outline", output)
+        output = orig.copy()
+        receiptCnt = receiptBounds.reshape(4,1,2).round().astype(int)
+        cv2.drawContours(output, [receiptCnt], -1, (0, 255, 0), 5)
+        cv2.imshow("Receipt Outline", imutils.resize(output, width=resize_width))
         cv2.imshow("Receipt Transform", imutils.resize(receipt, width=resize_width))
-        #cv2.imwrite("temp/01-orig.jpg",orig)
-        #cv2.imwrite("temp/02-reshaped.jpg",image)
-        #cv2.imwrite("temp/03a-gray.jpg",gray)
-        #cv2.imwrite("temp/03b-blurred.jpg",blurred)
-        #cv2.imwrite("temp/03c-edged.jpg",edged)
-        #cv2.imwrite("temp/04-Outline.jpg",output)
-        #cv2.imwrite("temp/05-ReceiptTransform.jpg",imutils.resize(receipt, width=resize_width))
+        
         cv2.waitKey()
         cv2.destroyAllWindows()
     return receipt
@@ -161,7 +172,7 @@ def test__extract_receipt():
 if __name__=='__main__':
     filename = 'data2\\220728.HEIC'
     #filename = 'data2\\220925.HEIC'#fail
-    #filename = 'data2\\221018.HEIC'#fail
+    filename = 'data2\\221018.HEIC'#fail
     #filename = 'data2\\221103.HEIC'#fail
     #filename = 'data2\\221118.HEIC'
     #filename = 'data2\\221209.HEIC'
@@ -169,8 +180,8 @@ if __name__=='__main__':
     #filename = 'data2\\230619.HEIC'
     #filename = 'data2\\230702.HEIC'
     #filename = 'data2\\230717.HEIC'
-    receipt = extract_receipt(filename, blur_size=5, resize_width=500)
-    print(receipt.shape)
+    receipt = extract_receipt(filename, blur_size=5, resize_width=300)
+    #print(receipt.shape)
     #cv2.imshow("Receipt", imutils.resize(receipt, width=300))
     #cv2.waitKey()
     #cv2.destroyAllWindows()
