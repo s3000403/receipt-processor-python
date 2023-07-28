7b953a38bf8a0e260906759a645fd46435086e29
Rob Schultz <59187877+s3000403@users.noreply.github.com>
2023-07-27 23:12:44 -0700

    Cleanup and comment helpers.py

--- helpers.py
+++ helpers.py
@@ -4,14 +4,18 @@ import cv2
 import numpy as np
 from PIL import Image
 from pillow_heif import register_heif_opener
+
+# Enable PIL.Image to open heif files
 register_heif_opener()
 
+# Convert between cv2 and PIL.Image formats
 def convert_from_cv2_to_image(img: np.ndarray) -> Image:
     return Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
 def convert_from_image_to_cv2(img: Image) -> np.ndarray:
     return cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
 
+# Image opening scripts
 def open_image( filename, astype='PIL' ):
     if astype.lower() in ['pil']:
         image = open_image_pil( filename )
